e8b3b6c8eca760cd652ff16c041b4478e4b5eca3
iwhoyoung <34534545+iwhoyoung@users.noreply.github.com>
2018-08-28 14:19:33 +0800

    add annotations
    add annotations

--- exceptionrecordlibrary/src/main/java/com/liyuanheng/www/exceptionrecordlibrary/ExceptionManager.java
+++ exceptionrecordlibrary/src/main/java/com/liyuanheng/www/exceptionrecordlibrary/ExceptionManager.java
@@ -2,7 +2,6 @@ package com.liyuanheng.www.exceptionrecordlibrary;
 
 import android.content.Context;
 import android.telephony.TelephonyManager;
-import android.util.Log;
 
 import com.liyuanheng.www.exceptionrecordlibrary.LocalFileHandler.OnExceptionCatchListener;
 
@@ -17,6 +16,10 @@ import java.util.Locale;
 import static com.liyuanheng.www.exceptionrecordlibrary.FileUtils.getDiskCacheDir;
 import static java.text.DateFormat.getDateTimeInstance;
 
+/**
+ * @author huyang
+ * @version 1.0.0
+ */
 public class ExceptionManager implements OnExceptionCatchListener {
 
     private DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.FULL, DateFormat.FULL, Locale.CHINA);
@@ -35,49 +38,102 @@ public class ExceptionManager implements OnExceptionCatchListener {
         Thread.setDefaultUncaughtExceptionHandler(new LocalFileHandler(mContext, this));
     }
 
+    /**
+     * 获取ExceptionManager实例化对象
+     * <p> 使用该方法去获取ExceptionManager实例化对象</p>
+     *
+     * @return ExceptionManager对象
+     */
     public static ExceptionManager getInstance() {
         return exceptionManager;
     }
 
+    /**
+     * 初始化方法
+     * <p>该方法一般放入Application的子类onCreate方法中，参数传入this即可</p>
+     *
+     * @param context 传入Context参数
+     * @return ExceptionManager实例化对象
+     */
     public static ExceptionManager init(Context context) {
         if (exceptionManager == null)
             exceptionManager = new ExceptionManager(context);
         return exceptionManager;
     }
 
+    /**
+     * 初始化方法
+     * <p>该方法一般放入Application的子类onCreate方法中
+     * <br>@param context传入this即可
+     * <br>@param isHost目前传入true即可，功能尚未完善</p>
+     *
+     * @param context 传入Context参数
+     * @param isHost  代表是否托管，按照默认配置执行
+     * @return ExceptionManager实例化对象
+     */
     public static ExceptionManager init(Context context, boolean isHost) {
         if (exceptionManager == null)
             exceptionManager = new ExceptionManager(context);
         return exceptionManager;
     }
 
+    /**
+     * 使异常捕捉本地保存功能开始工作
+     * <p>一般在配置完毕后，调用该方法，随后，异常捕捉保存才真正开始工作</p>
+     */
     public void start() {
         if (recordFile == null)
             recordFile = new File(getDiskCacheDir(mContext) + "/log/");
         String path = recordFile.getAbsolutePath();
         if (!recordFile.exists())
-             recordFile.mkdirs();
+            recordFile.mkdirs();
 
         if (FileUtils.getFileOrFolderSize(path) > storageLimit) {
             FileUtils.deleteAll(path);
         }
     }
 
+    /**
+     * 设置保存路径
+     *
+     * @param file 保存的文件路径
+     * @return ExceptionManager实例化对象
+     */
     public ExceptionManager setStoragePath(File file) {
         recordFile = file;
         return exceptionManager;
     }
 
+    /**
+     * 设置保存路径
+     *
+     * @param path 保存的文件路径
+     * @return ExceptionManager实例化对象
+     */
     public ExceptionManager setStoragePath(String path) {
         recordFile = new File(path);
         return exceptionManager;
     }
 
+    /**
+     * 设置时区
+     *
+     * @param aLocale 所处地区
+     * @return ExceptionManager实例化对象
+     */
     public ExceptionManager setTimezone(Locale aLocale) {
         dateFormat = DateFormat.getDateTimeInstance(DateFormat.FULL, DateFormat.FULL, aLocale);
         return exceptionManager;
     }
 
+    /**
+     * 设置异常发生时，Toast提示内容
+     * <p>设置异常发生时，对用户的Toast提示内容</p>
+     *
+     * @param content          Toast提示内容
+     * @param isToastIncludeEx 是否在Toast内容末尾附上异常内容
+     * @return ExceptionManager实例化对象
+     */
     public ExceptionManager setExceptionToast(String content, boolean isToastIncludeEx) {
         exceptionTip = content;
         isToastEx = isToastIncludeEx;
@@ -86,20 +142,31 @@ public class ExceptionManager implements OnExceptionCatchListener {
 
     /**
      * 初始化检测，大于该值时，清理文件夹,默认3M
+     * <p>在调用start()方法时，会检测缓存异常日志文件缓存大小，超过限制时，将清空文件夹</p>
      *
      * @param limit 单位KB
-     * @return
+     * @return ExceptionManager实例化对象
      */
     public ExceptionManager setStorageLimit(int limit) {
         storageLimit = limit;
         return exceptionManager;
     }
 
+    /**
+     * 设置保存异常文件为单一文件还是多个文件
+     *
+     * @param isSingleStorageFile 是否为单一文件
+     * @return ExceptionManager实例化对象
+     */
     public ExceptionManager setSingleFile(boolean isSingleStorageFile) {
         this.isSingleStorageFile = isSingleStorageFile;
         return exceptionManager;
     }
 
+    /**
+     * @param ex 被捕获的异常
+     * @return
+     */
     @Override
     public String OnExceptionCatchToast(Throwable ex) {
         if (exceptionTip != null)
@@ -110,6 +177,9 @@ public class ExceptionManager implements OnExceptionCatchListener {
         return ex.toString();
     }
 
+    /**
+     * @param ex 被捕获的异常
+     */
     @Override
     public void OnExceptionCatch(Throwable ex) {
         saveLog(ex);
